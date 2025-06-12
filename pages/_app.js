61f9c386a46e95bde26cb8b8d2c1d32dfdfabba4
Yousef eslam <yousefeslam214@gmail.com>
2025-06-13 02:32:23 +0300

    improve some 'Best Practices'

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,8 @@ export default function App({ Component, pageProps }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           media="print"
           onLoad="this.media='all'"
-          integrity="sha512-pOa8I7fg1eZxGzZ+8cLlfkQECujloW1sPqA2t82z6+9fRvkPqELBzmZ6h7TvRPHB+Er8t5TVh0V3R9a+2T2vXg=="
+          // integrity="sha512-pOa8I7fg1eZxGzZ+8cLlfkQECujloW1sPqA2t82z6+9fRvkPqELBzmZ6h7TvRPHB+Er8t5TVh0V3R9a+2T2vXg=="
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
